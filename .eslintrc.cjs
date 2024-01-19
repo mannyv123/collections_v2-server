542e95855720ddcf656b391a79071261eb08bbb5
Manjot Virdi <virdi_manjot@hotmail.com>
2024-01-19 00:57:46 -0800

    update format

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,19 @@
 module.exports = {
-    root: true,
-    extends: [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended-type-checked",
-        "prettier",
-        "plugin:prettier/recommended",
-    ],
-    ignorePatterns: ["dist", ".eslintrc.cjs"],
-    parser: "@typescript-eslint/parser",
-    plugins: ["@typescript-eslint"],
-    parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
-        project: ["./tsconfig.json"],
-        extraFileExtensions: [".json"],
-        tsconfigRootDir: __dirname,
-    },
+   root: true,
+   extends: [
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended-type-checked",
+      "prettier",
+      "plugin:prettier/recommended",
+   ],
+   ignorePatterns: ["dist", ".eslintrc.cjs"],
+   parser: "@typescript-eslint/parser",
+   plugins: ["@typescript-eslint"],
+   parserOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      project: ["./tsconfig.json"],
+      extraFileExtensions: [".json"],
+      tsconfigRootDir: __dirname,
+   },
 };
