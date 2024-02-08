6c9244cd5003b5fc4cabb7ae405e2d792849bc30
Manjot Virdi <virdi_manjot@hotmail.com>
2024-02-07 20:44:14 -0800

    set up initial server boilerplate

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,17 @@
-console.log("hello");
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+// Config
+dotenv.config();
+const PORT = process.env.PORT || 5001;
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// Routes
+
+// Server start
+app.listen(PORT, () => {
+   console.log(`Express server listening on port: ${PORT}`);
+});
