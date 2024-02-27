f83542a4852d0b4282f7aefc21457e7c570f3732
Manjot Virdi <virdi_manjot@hotmail.com>
2024-02-26 20:28:50 -0800

    update seeds

---
+++ src/lib/db/seed_data/likes.ts
@@ -0,0 +1,14 @@
+const likesData = [
+   {
+      id: "174875ee-e638-8989-9857-40f861c965c",
+      image_id: "144875fe-e638-4dea-9857-40f861c9774c",
+      user_id: "174875ee-e638-4dea-9857-40f861c9774c",
+   },
+   {
+      id: "184875ee-e638-02389-9857-40f861c909c",
+      image_id: "144875fe-e638-4dea-9857-40f861c9774c",
+      user_id: "184875ee-e638-4dea-9857-40f861c9774c",
+   },
+];
+
+export default likesData;
